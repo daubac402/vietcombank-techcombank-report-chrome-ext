e73a5a1f6e04f8badccfc1588c114ec1bbcc83c2
The Anh <daubac403@gmail.com>
2021-01-28 00:43:16 +0900

    temp (not done)

--- js/main.js
+++ js/main.js
@@ -1,61 +1,45 @@
-var isFirstRun = true;
+var lastReportDom = null;
 
-jsCopyToClipboard = function(){
-    console.log('🚀 ~ file: main.js ~ line 59 ~ $ ~ $("div.js-quick-report p").html()', $("div.js-quick-report p").html())
-	$("div.js-quick-report p").html().select();
-	document.execCommand("Copy");
-};
+function jsCopyToClipboard(element) {
+	var $temp = $("<textarea></textarea>");
+	$("body").append($temp);
+	$temp.html($(element).html().replace(/<br[^>]*>/g, "\n")).select();
+	document.execCommand("copy");
+	$temp.remove();
+}
 
 function runFunction() {
-	var report_main = `<div class="js-quick-report">
+	var reportMain = `<div class="js-quick-report">
 						   <h1>Quick Report</h1>
-						   <button onClick="jsCopyToClipboard">Copy to Clipboard</button>
+						   <button class='js-copy-to-clipboard'">Copy to Clipboard</button>
 						   <p>`;
-	$("#toanbo > div > div").each(function(){
+	$("#toanbo > div > div").each(function () {
 		let $this = $(this);
-		report_main += $this.find("div > div:nth-child(1) > div:nth-child(1)").html() + "<br>";
-		report_main += $this.find("div > div:nth-child(1) > div:nth-child(2)").html() + "<br>";
-		report_main += $this.find("div > div:nth-child(2) > div:nth-child(2)").html() + "<br>";
-		report_main += "<br>";
+		let money = $this.find("div > div:nth-child(2) > div:nth-child(2)").html();
+		if (money && !money.includes("-")) {
+			reportMain += $this.find("div > div:nth-child(1) > div:nth-child(1)").html() + "<br>";
+			reportMain += $this.find("div > div:nth-child(1) > div:nth-child(2)").html() + "<br>";
+			reportMain += money + "<br>";
+			reportMain += "<br>";
+		}
 	});
-	report_main += "</p></div>";
-	
-	if (isFirstRun) {
+	reportMain += "</p></div>";
+
+	if (!lastReportDom) {
 		if ($("#ChiTietTaiKhoan > div > div > div.box.no-p").length > 0) {
-			$("#ChiTietTaiKhoan > div > div > div.box.no-p").before(report_main);
-			isFirstRun = false;
+			$("#ChiTietTaiKhoan > div > div > div.box.no-p").before(reportMain);
+			lastReportDom = reportMain;
 		}
 	} else {
-		$("div.js-quick-report").remove();
-		$("#ChiTietTaiKhoan > div > div > div.box.no-p").before(report_main);
+		if (lastReportDom != reportMain) {
+			$("div.js-quick-report").remove();
+			$("#ChiTietTaiKhoan > div > div > div.box.no-p").before(reportMain);
+			lastReportDom = reportMain;
+		}
 	}
 }
-setInterval(runFunction, 1000);
+setInterval(runFunction, 500);
 
-// $("label#ST2_Challenge").css({ "color": "#8cc63e", "font-size": "40px" });
-
-// var changeEvent = document.createEvent("HTMLEvents");
-// changeEvent.initEvent("change", true, true);
-
-// $("input#SoTaiKhoanNguoiHuong").blur(() => {
-// 	$("select#LoaiNganHang").val('0');
-// 	$("select#LoaiNganHang")[0].dispatchEvent(changeEvent);
-// });
-// $("select#LoaiNganHang").select2().on('select2:select', function (e) {
-// 	$(this)[0].dispatchEvent(changeEvent);
-// });
-// $("select#TenNganHangHuong").select2().on('select2:select', function (e) {
-// 	$(this)[0].dispatchEvent(changeEvent);
-// });
-// $("select#TinhThanh").select2().on('select2:select', function (e) {
-// 	$(this)[0].dispatchEvent(changeEvent);
-// });
-// $("select#ChiNhanh").select2().on('select2:select', function (e) {
-// 	$(this)[0].dispatchEvent(changeEvent);
-// });
-
-// $("select#HinhThucChuyenTien").change(function() {
-// 	$("select#MaNganHangHuong").select2().on('select2:select', function (e) {
-// 		$(this)[0].dispatchEvent(changeEvent);
-// 	});
-// });
+$(document).on("click", ".js-copy-to-clipboard", function() {
+	jsCopyToClipboard("div.js-quick-report p");
+});
